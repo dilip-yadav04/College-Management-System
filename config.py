e9fa910b8e04efe8aeafedf79d844d6455102990
dilip-yadav04 <dilipsin729@gmail.com>
2026-07-08 00:37:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = 'college_management_system_secret_key_2026'
+
+    MYSQL_HOST     = 'localhost'
+    MYSQL_USER     = 'root'
+    MYSQL_PASSWORD = 'Rovino@1204'   # ← put your actual MySQL password here
+    MYSQL_DATABASE = 'college_management'
+    MYSQL_PORT     = 3306
